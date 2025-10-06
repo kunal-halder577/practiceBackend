2eabfcb73a6e65cba87bf064939b6cff0d0042a8
Kunal <kunalhalder577@gmail.com>
2025-10-06 21:37:35 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Welcome");
+})
+app.get("/native", (_, res) => {
+    res.send({
+        "id": "hdk31ka8392uqp12bk",
+        "name": "Kunal Halder"
+    })
+})
+app.listen(process.env.PORT, () => {
+    console.log(`server is listening on port ${process.env.PORT}`);
+})
